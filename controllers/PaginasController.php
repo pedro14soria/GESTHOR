c294527cb57a9e839cbf04e6b2489aed16db817e
Pedro Soria <pedro.soria@mi.unc.edu.ar>
2025-12-10 11:47:00 -0300

    Cambios para que se cierre correctamente la sesion

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -7,12 +7,16 @@ use MVC\Router;
 class PaginasController {
     public static function inicio(Router $router)
     {
+        session_start();
+        $_SESSION = [];
         $router->render('auth/index', [
             'titulo' => 'Inicio'
         ]);
     }
 
     public static function error(Router $router) {
+        session_start();
+        $_SESSION = [];
         $router->render('auth/error', [
             'titulo' => 'ERROR 404'
         ]);
